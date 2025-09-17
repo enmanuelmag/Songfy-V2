7daf6b6892a4007f3cd0b13f357df9e1978b1698
enmanuelmag <enmanuelmag@cardor.dev>
2025-09-17 07:49:39 -0500

    feat: enhance user and spotify slices with additional state and actions for improved store management

--- store/index.ts
+++ store/index.ts
@@ -1,102 +1,158 @@
-import moment from 'moment';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-import { CurrencyOptions } from '@constants/currency';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import type { CurrencyType } from '@constants/currency';
+import type { RoutesType } from '@constants/routes';
+import type { NotificationForegroundType } from '@customTypes/notification';
+import type {
+  PlaylistType,
+  SongLyricsStoreType,
+  SongLyricsType,
+} from '@customTypes/spotify';
 import type { UserType } from '@customTypes/user';
-import type { NotificationPermissionsStatus } from 'expo-notifications';
 
-type ThemeOptions = 'light' | 'dark';
+type ThemeOptions = 'light' | 'dark' | string;
 
+// User slice
 type UserSlice = {
   // states
+  tabSelected: RoutesType;
   user: UserType | null;
   theme: ThemeOptions;
   usedSystemTheme: boolean;
-  currency: CurrencyType;
-  lastCheckUpdate: number | null;
-  activeTimestamp: number | null;
-  backgroundTimestamp: number | null;
-  notificationSettings: NotificationPermissionsStatus | null;
+  pushToken?: string | null;
+  popOverNotification?: NotificationForegroundType | null;
   tokenExpired: boolean;
   tokenSentAt?: number;
+};
+
+type UserSliceActions = {
   // actions
   clear: () => void;
-  setRequiredBiometric: (requiredBiometric: boolean) => void;
-  setUsedSystemTheme: (usedSystemTheme: boolean) => void;
-  setLastCheckUpdate: (lastCheckUpdate: number) => void;
   setUser: (user?: UserType | null) => void;
   setTheme: (theme: ThemeOptions) => void;
-  setCurrency: (currency: CurrencyType) => void;
-  setNotificationSettings: (
-    notificationSettings: NotificationPermissionsStatus | null
+  setUsedSystemTheme: (usedSystemTheme: boolean) => void;
+  setTabSelected: (tabSelected: UserSlice['tabSelected']) => void;
+  setPushToken: (pushToken: string | null) => void;
+  setPopOverNotification: (
+    popOverNotification: NotificationForegroundType | null
   ) => void;
   setTokenExpired: (tokenExpired: boolean) => void;
   setTokenSentAt: (tokenSentAt?: number) => void;
 };
 
-type BudgetSlice = {
-  // states
-  // Budget form
-  // Charge
-  // transversal actions
-  clear: () => void;
-};
-
-const initialUserSlice = {
+const initialUserSlice: UserSlice = {
   user: null,
-  theme: 'light' as ThemeOptions,
+  theme: 'light',
+  tabSelected: '/search',
   usedSystemTheme: false,
-  activeTimestamp: null,
-  lastCheckUpdate: null,
-  backgroundTimestamp: null,
-  notificationSettings: null,
-  currency: CurrencyOptions[0],
+  pushToken: null,
+  popOverNotification: null,
   tokenExpired: false,
-  tokenSentAt: undefined,
 };
 
-const initialBudgetSlice = {};
+// Spotify slice
+type SpotifySlice = {
+  // states
+  playlists: Array<PlaylistType>;
+  songsLyrics: SongLyricsStoreType;
+  searchedSongs: Array<SongLyricsType>;
+  searchBrief: string;
+  searchKeywords: Array<string>;
+  isLimitEnabled: boolean;
+  stepTextAnalysis?: string | null;
+};
+
+type SpotifySliceActions = {
+  // actions
+  clearPlaylists: () => void;
+  setPlaylists: (playlists: SpotifySlice['playlists']) => void;
+  setSongsLyrics: (songsLyrics: SpotifySlice['songsLyrics']) => void;
+  putSongLyrics: (songLyrics: SongLyricsType) => void;
+  setSearchedSongs: (searchedSongs: SpotifySlice['searchedSongs']) => void;
+  setSearchBrief: (searchBrief: string) => void;
+  setSearchKeywords: (searchKeywords: Array<string>) => void;
+  removeSongLyrics: (songId: string) => void;
+  clearSongsLyrics: () => void;
+  setLimitEnabled: (isLimitEnabled: boolean) => void;
+  setStepTextAnalysis: (stepTextAnalysis: string | null) => void;
+};
+
+const initialSpotifySlice: SpotifySlice = {
+  playlists: [],
+  songsLyrics: {},
+  searchedSongs: [],
+  searchBrief: '',
+  searchKeywords: [],
+  isLimitEnabled: false,
+  stepTextAnalysis: undefined,
+};
+
+type StoreType = UserSlice &
+  UserSliceActions &
+  SpotifySlice &
+  SpotifySliceActions;
 
 export const useAppStore = create(
-  persist<UserSlice & BudgetSlice>(
+  persist<StoreType>(
     (set) => ({
       ...initialUserSlice,
-      ...initialBudgetSlice,
-      // User actions
+      ...initialSpotifySlice,
+      // UserSlice Actions
+      clear: () => set({ ...initialUserSlice, ...initialSpotifySlice }),
       setUser: (user) => set({ user }),
-      setRequiredBiometric: (requiredBiometric) =>
-        set((prev) => {
-          if (requiredBiometric) {
-            return { ...prev, backgroundTimestamp: moment().unix() };
-          }
-          return { ...prev, activeTimestamp: moment().unix() };
-        }),
       setTheme: (theme) => set({ theme }),
-      setCurrency: (currency) => set({ currency }),
-      setLastCheckUpdate: (lastCheckUpdate) => set({ lastCheckUpdate }),
       setUsedSystemTheme: (usedSystemTheme) => set({ usedSystemTheme }),
-      setNotificationSettings: (notificationSettings) =>
-        set({ notificationSettings }),
-      // Budget actions
-      // Transversal actions
-      clear: () =>
-        set((state) => ({
-          ...initialUserSlice,
-          ...initialBudgetSlice,
-          theme: state.theme,
-        })),
-
+      setTabSelected: (tabSelected) => set({ tabSelected }),
+      setPushToken: (pushToken) => set({ pushToken }),
+      setPopOverNotification: (popOverNotification) =>
+        set({ popOverNotification }),
       setTokenExpired: (tokenExpired) => set({ tokenExpired }),
       setTokenSentAt: (tokenSentAt) => set({ tokenSentAt }),
+      // Spotify Actions
+      setPlaylists: (playlists) => set({ playlists }),
+      clearPlaylists: () => set({ playlists: [] }),
+      setSongsLyrics: (songsLyrics) => set({ songsLyrics }),
+      putSongLyrics: (songLyrics) =>
+        set((state) => ({
+          songsLyrics: { ...state.songsLyrics, [songLyrics.id]: songLyrics },
+        })),
+      setSearchedSongs: (searchedSongs) => set({ searchedSongs }),
+      setSearchBrief: (searchBrief) => set({ searchBrief }),
+      setSearchKeywords: (searchKeywords) => set({ searchKeywords }),
+      removeSongLyrics: (songId) =>
+        set((state) => {
+          const { [songId]: _, ...rest } = state.songsLyrics;
+          return { songsLyrics: rest };
+        }),
+      clearSongsLyrics: () => set({ songsLyrics: {} }),
+      setLimitEnabled: (isLimitEnabled) => set({ isLimitEnabled }),
+      setStepTextAnalysis: (stepTextAnalysis) => set({ stepTextAnalysis }),
     }),
     {
       version: 1,
-      name: 'app-songfy-store',
+      name: 'app-store',
       storage: createJSONStorage(() => AsyncStorage),
+      migrate(persistedState, oldVersion) {
+        const castedPersistedState = persistedState as StoreType;
+
+        if (oldVersion === 3) {
+          return {
+            ...castedPersistedState,
+            songsLyrics: {},
+          };
+        }
+
+        const newState =
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          castedPersistedState || {
+            ...initialUserSlice,
+            ...initialSpotifySlice,
+          };
+
+        return newState;
+      },
     }
   )
 );
